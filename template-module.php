36736103174a7da0987c9da7c3ac3a88b8e801a8
Andrew Minion <andrew@andrewrminion.com>
2016-01-06 14:46:13 -0500

    add template for module pages

---
+++ template-module.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Template name: Module Page
+ * The template for displaying pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that
+ * other "pages" on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Sixteen
+ * @since Twenty Sixteen 1.0
+ */
+
+get_header(); ?>
+
+<div id="primary" class="content-area no-sidebar">
+    <main id="main" class="site-main" role="main">
+        <?php
+        // Start the loop.
+        while ( have_posts() ) : the_post();
+        the_post_thumbnail( 'header-image' );
+        the_title();
+
+            // Include the page content template. ?>
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <div class="entry-content">
+                    <?php
+                    the_content();
+                    ?>
+                </div><!-- .entry-content -->
+
+                <?php
+                    edit_post_link(
+                        sprintf(
+                            /* translators: %s: Name of current post */
+                            __( 'Edit<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
+                            the_title( '', '', false )
+                        ),
+                        '<footer class="entry-footer"><span class="edit-link">',
+                        '</span></footer><!-- .entry-footer -->'
+                    );
+                ?>
+
+            </article><!-- #post-## -->
+
+            <?php
+            // End of the loop.
+        endwhile;
+        ?>
+
+    </main><!-- .site-main -->
+
+    <?php get_sidebar( 'content-bottom' ); ?>
+
+</div><!-- .content-area -->
+
+<?php get_footer(); ?>
