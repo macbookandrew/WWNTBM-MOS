6db4d8d4e6739ff322c73c0051fd319886b28e58
Andrew Minion <andrew@andrewrminion.com>
2016-01-06 11:31:52 -0500

    add .no-sidebar styles

--- page-home.php
+++ page-home.php
@@ -13,7 +13,7 @@
 
 get_header(); ?>
 
-<div id="primary" class="content-area">
+<div id="primary" class="content-area no-sidebar">
     <main id="main" class="site-main" role="main">
         <?php
         // Start the loop.
