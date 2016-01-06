25a956de623ca5334910b0b2ff7cdfaceee0d08f
Andrew Minion <andrew@andrewrminion.com>
2016-01-06 14:45:20 -0500

    normalize whitespace

--- functions.php
+++ functions.php
@@ -9,24 +9,24 @@ add_action( 'wp_enqueue_scripts', 'add_theme_styles' );
 
 // tweak default fonts
 function twentysixteen_fonts_url() {
-	$fonts_url = '';
-	$fonts     = array();
-	$subsets   = 'latin,latin-ext';
+    $fonts_url = '';
+    $fonts     = array();
+    $subsets   = 'latin,latin-ext';
 
-	if ( 'off' !== _x( 'on', 'Merriweather font: on or off', 'twentysixteen' ) ) {
-		$fonts[] = 'Merriweather:400,700,300italic,400italic,700italic';
-	}
+    if ( 'off' !== _x( 'on', 'Merriweather font: on or off', 'twentysixteen' ) ) {
+        $fonts[] = 'Merriweather:400,700,300italic,400italic,700italic';
+    }
 
-	if ( 'off' !== _x( 'on', 'Montserrat font: on or off', 'twentysixteen' ) ) {
-		$fonts[] = 'Montserrat:400,700';
-	}
+    if ( 'off' !== _x( 'on', 'Montserrat font: on or off', 'twentysixteen' ) ) {
+        $fonts[] = 'Montserrat:400,700';
+    }
 
-	if ( $fonts ) {
-		$fonts_url = add_query_arg( array(
-			'family' => urlencode( implode( '|', $fonts ) ),
-			'subset' => urlencode( $subsets ),
-		), 'https://fonts.googleapis.com/css' );
-	}
+    if ( $fonts ) {
+        $fonts_url = add_query_arg( array(
+            'family' => urlencode( implode( '|', $fonts ) ),
+            'subset' => urlencode( $subsets ),
+        ), 'https://fonts.googleapis.com/css' );
+    }
 
-	return $fonts_url;
+    return $fonts_url;
 }
