16aa2e295155f476f49996d363f1fd53d9499f28
Andrew Minion <andrew@andrewrminion.com>
2016-01-05 21:30:04 -0500

    remove sidebar and add page content

--- page-home.php
+++ page-home.php
@@ -19,14 +19,27 @@ get_header(); ?>
         // Start the loop.
         while ( have_posts() ) : the_post();
 
-            // Include the page content template.
-            get_template_part( 'template-parts/content', 'page' );
+            // Include the page content template. ?>
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <div class="entry-content">
+                    <?php the_content(); ?>
+                </div><!-- .entry-content -->
 
-            // If comments are open or we have at least one comment, load up the comment template.
-            if ( comments_open() || get_comments_number() ) {
-                comments_template();
-            }
+                <?php
+                    edit_post_link(
+                        sprintf(
+                            /* translators: %s: Name of current post */
+                            __( 'Edit<span class="screen-reader-text"> "%s"</span>', 'twentysixteen' ),
+                            the_title( '', '', false )
+                        ),
+                        '<footer class="entry-footer"><span class="edit-link">',
+                        '</span></footer><!-- .entry-footer -->'
+                    );
+                ?>
 
+            </article><!-- #post-## -->
+
+            <?php
             // End of the loop.
         endwhile;
         ?>
@@ -37,5 +50,4 @@ get_header(); ?>
 
 </div><!-- .content-area -->
 
-<?php get_sidebar(); ?>
 <?php get_footer(); ?>
