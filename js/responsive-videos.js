ef6e9e06ae8964517b12fdf05678e7ad92bfd982
Andrew Minion <andrew@andrewrminion.com>
2018-06-07 12:30:39 -0400

    add sticky video when scrolling

--- js/responsive-videos.js
+++ js/responsive-videos.js
@@ -1,37 +1,45 @@
-// Find all YouTube and Vimeo videos; from https://css-tricks.com/NetMag/FluidWidthVideo/Article-FluidWidthVideo.php
-jQuery(document).ready(function($) {
-    var $allVideos = $("iframe[src^='https://player.vimeo.com'], iframe[src^='https://www.youtube.com']"),
+(function($) {
+    // See https://css-tricks.com/NetMag/FluidWidthVideo/Article-FluidWidthVideo.php
+    $(document).ready(function($) {
+        // Get all videos.
+        var $allVideos = $("iframe[src^='https://player.vimeo.com'], iframe[src^='https://www.youtube.com']"),
+            $fluidEl = $(".entry-content"), // the fluid-width element.
+            firstVideo = $allVideos.get(0),
+            $firstVideoWrap = $(firstVideo).parents('p')
 
-        // The element that is fluid width
-        $fluidEl = $(".entry-content");
+        // Save aspect ratio as data attributes for each video.
+        $allVideos.each(function() {
+          $(this).data('aspectRatio', this.height / this.width).removeAttr('height').removeAttr('width');
+        });
 
-    // Figure out and save aspect ratio for each video
-    $allVideos.each(function() {
+        // Preserve aspect ratio on window resize.
+        $(window).resize(function() {
 
-      $(this)
-        .data('aspectRatio', this.height / this.width)
+          var newWidth = $fluidEl.width();
 
-        // and remove the hard coded width/height
-        .removeAttr('height')
-        .removeAttr('width');
+          // Resize all videos according to their own aspect ratio
+          $allVideos.each(function() {
+            var $el = $(this),
+                aspectRatio = $el.data('aspectRatio')
+            $el.width(newWidth).height(newWidth * aspectRatio);
+          });
 
+        // Kick off one resize to fix all videos on page load
+        }).resize();
+
+        // Keep video in view on page scroll.
+        $(window).on('scroll', function() {
+            var videoHeight = $firstVideoWrap.outerHeight(),
+                windowScrollTop = $(window).scrollTop(),
+                videoMiddle = (videoHeight / 2) + $firstVideoWrap.offset().top;
+
+            if (windowScrollTop > videoMiddle) {
+                $firstVideoWrap.height(videoHeight);
+                $(firstVideo).addClass('stuck');
+            } else {
+                $firstVideoWrap.height('auto');
+                $(firstVideo).removeClass('stuck');
+            }
+        });
     });
-
-    // When the window is resized
-    $(window).resize(function() {
-
-      var newWidth = $fluidEl.width();
-
-      // Resize all videos according to their own aspect ratio
-      $allVideos.each(function() {
-
-        var $el = $(this);
-        $el
-          .width(newWidth)
-          .height(newWidth * $el.data('aspectRatio'));
-
-      });
-
-    // Kick off one resize to fix all videos on page load
-    }).resize();
-});
+}(jQuery));
